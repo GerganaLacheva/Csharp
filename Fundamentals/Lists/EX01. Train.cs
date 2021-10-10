7d52ef18b236756d67477dce02102bc656f358d5
Gergana Lacheva <90899782+GerganaLacheva@users.noreply.github.com>
2021-10-10 11:26:52 +0300

    Update EX01. Train.cs

--- Fundamentals/Lists/EX01. Train.cs
+++ Fundamentals/Lists/EX01. Train.cs
@@ -45,3 +45,30 @@ namespace EX01._Train
 
     }
 }
+//1.Train
+//You On the first line, we will receive a list of wagons (integers). Each integer represents the number of passengers that are currently in each wagon of a passenger train. On the next line, you will receive the max capacity of a wagon represented as a single integer. Until you receive the "end" command, you will be receiving two types of input:
+//•	Add
+//{ passengers} – add a wagon to the end of the train with the given number of passengers.
+//•	{passengers} -find a single wagon to fit all the incomming passengers (starting from the first wagon).
+//In the end, print the final state of the train (all the wagons separated by a space).
+//Example
+//Input	                        Output
+//32 54 21 12 4 0 23
+//75
+//Add 10
+//Add 0
+//30
+//10
+//75
+//end	                        72 54 21 12 4 75 23 10 0
+//
+//Input	                        Output
+//0 0 0 10 2 4
+//10
+//Add 10
+//10
+//10
+//10
+//8
+//6
+//end	                        10 10 10 10 10 10 10
