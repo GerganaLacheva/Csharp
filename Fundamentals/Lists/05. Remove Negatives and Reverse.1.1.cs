34099cb0150442b4d1972ae86333c93ff54b862d
Gergana Lacheva <90899782+GerganaLacheva@users.noreply.github.com>
2021-11-24 09:57:48 +0200

    Create 05. Remove Negatives and Reverse.1.1.cs

---
+++ Fundamentals/Lists/05. Remove Negatives and Reverse.1.1.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Remove_Negatives_and_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> result = new List<int>();
+
+            foreach (var element in numbers)
+            {
+                if (element > 0)
+                {
+                    result.Add(element);
+                }
+            }
+            result.Reverse();
+
+            if (result.Count==0)
+            {
+                Console.WriteLine("empty");
+            }
+            Console.WriteLine(string.Join(" ",result));
+        }
+    }
+}
+//5.Remove Negatives and Reverse
+//Read a list of integers, remove all negative numbers from it and print the remaining elements in reversed order. If there are no elements left in the list, print "empty".
+//Examples
+//Input	                    Output
+//10 -5 7 9 -33 50	        50 9 7 10
+//7 -2 -10 1	            1 7
+//-1 -2 -3	                empty
