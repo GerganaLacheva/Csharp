82ab38853484ab06b6ab1c8a7bcb86bbc6450b5a
Gergana Lacheva <90899782+GerganaLacheva@users.noreply.github.com>
2021-12-14 09:50:38 +0200

    Update 2. Sum Matrix Columns.cs

--- Advanced/02.Multidimensional Arrays/2. Sum Matrix Columns.cs
+++ Advanced/02.Multidimensional Arrays/2. Sum Matrix Columns.cs
@@ -1,30 +1,59 @@
 using System;
 using System.Linq;
- 
-namespace Sum_Matrix_Columns
+
+namespace _2._Sum_Matrix_Columns
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            string line = Console.ReadLine();
-            int[] size = line.Split(',').Select(int.Parse).ToArray();
-            int rows = size[0];
-            int columns = size[1];
-            int[] sumColumns = new int[columns];
-                for (int i = 0; i < rows; i++)
+            int[] dimensions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[dimensions[0], dimensions[1]];
+
+            int[] sum = new int[dimensions[1]];
+
+            for (int i = 0; i < dimensions[0]; i++)
             {
-                string currentLine = Console.ReadLine();
-                int[] currentArray = currentLine.Split().Select(int.Parse).ToArray();
-                for (int j = 0; j <columns; j++)
+                int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < dimensions[1]; j++)
                 {
-                    sumColumns[j] += currentArray[j];
+                    matrix[i, j] = arr[j];
+                    sum[j]+=arr[j];
                 }
             }
-            foreach (int number in sumColumns)
+            foreach (var item in sum)
             {
-                Console.WriteLine(number);
+                Console.WriteLine(item);
             }
         }
     }
 }
+//2.Sum Matrix Columns
+//Create a program that read a matrix from console and print the sum for each column. On first line you will get matrix rows. On the next rows lines, you will get elements for each column separated with a space. 
+//Examples
+//Input	
+//3, 6
+//7 1 3 3 2 1
+//1 3 9 8 5 6
+//4 6 7 9 1 0	
+//
+//Output
+//12
+//10
+//19
+//20
+//8
+//7
+//
+//Input
+//3, 3
+//1 2 3
+//4 5 6
+//7 8 9	
+//
+//Output
+//12
+//15
+//18
+
